67e6f4c1593fb0f355382f3118ee54bf6002a95c
thaseen <fathimatj@hotmail.com>
2022-09-10 18:43:20 -0700

    assignment complete

---
+++ assignment.js
@@ -0,0 +1,13 @@
+const userInput = 10;
+let initialValue;
+
+initialValue = 18 + userInput;
+
+initialValue = initialValue - 8;
+initialValue = initialValue * 9;
+initialValue = initialValue / 2;
+
+/* The value stored in the user input variable didn't change*/
+
+alert(userInput);
+alert(initialValue);
